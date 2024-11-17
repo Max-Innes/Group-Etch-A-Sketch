3cb7b5308b298a89f7bcf18207d99e461829f62f
notnycee <cocogaming23@gmail.com>
2024-11-16 22:57:45 -0500

    Pen_control_code
    updated some of the skeletal code and new file is the filled in skeletal code

---
+++ Pen_control_code.py
@@ -0,0 +1,46 @@
+from machine import Pin, ADC, PWM
+import time
+
+'''initializing variables'''
+#start with pen down as the default
+pen_position = False
+servo = None #will be initialized later on in the rest of the code
+
+# main is the main function that wil run the entire pen positioning program
+def pen_control():
+    initialize_servo()
+    read_position()
+    switch_position()
+
+#function to set up the servo at an initial position.
+def initialize_servo():  
+    servo = PWM(Pin(0)) # servo
+    servo.freq(50)
+    servo.set_servo_angle(0)
+    print("Pen Servo initialized at position: Down")
+''' input: assign servo to a pin and assign initial position
+output: return servo to initial position'''
+
+#function to read the current position
+def read_position(current_position):
+     return pen_position
+
+'''Input: read input to know the current position of the servo
+if current position is False(down) or True(up)'''
+    
+'''output : returns boolean value -> the current position of the servo'''
+        
+#function to switch position
+def switch_position():
+    if pen_position:
+        servo.set_servo_angle(0)
+        pen_position = False
+        print("Pen moved to position: Down")
+    else:
+        servo.set_servo_angle(90)
+        pen_position = True
+        print("Pen moved to position: Up")
+'''input : current position is read
+output : program switches current state to opposite state '''
+
+pen_control() #this runs the main program above
--- Pen_control_skeletal_code.py
+++ Pen_control_skeletal_code.py
@@ -4,16 +4,20 @@ import time
 '''initializing variables'''
 #start with pen down as the default
 pen_position = False
-servo = none #will be initialized later
+servo = None #will be initialized later on in the program
 
 # main is the main function that wil run the entire pen positioning program
-def main():
+def pen_control():
 initialize_servo()
 read_position()
 switch_position()
 
 #function to set up the servo at an initial position.
 def initialize_servo():
+   
+   servo = PWM(Pin(0)) # servo
+    
+
    ''' input: assign servo to a pin and assign initial position
     output: return servo to initial position'''
 
@@ -29,4 +33,4 @@ def switch_position():
     '''input : current position is read
         output : program switches current state to opposite state '''
 
-main() #this runs the main program above
+pen_control() #this runs the main program above
